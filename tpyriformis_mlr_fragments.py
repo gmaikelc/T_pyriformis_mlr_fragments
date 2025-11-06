0e2bfe831238c8c61fe10c4d848e73761e905b32
Gerardo Casanola <39452660+gmaikelc@users.noreply.github.com>
2025-11-06 11:09:49 -0600

    Update tpyriformis_mlr_fragments.py

--- tpyriformis_mlr_fragments.py
+++ tpyriformis_mlr_fragments.py
@@ -723,7 +723,7 @@ def make_fragment_grid_for_molecule(
         b_colors = {i: color for i in b_list}
 
         mols.append(mol)
-        legends.append(f"{label} (, {_tox_phrase(sign)})")
+        legends.append(f"{label} ({_tox_phrase(sign)})")
         highlightAtomLists.append(a_list); highlightBondLists.append(b_list)
         highlightAtomColors.append(a_colors); highlightBondColors.append(b_colors)
 
@@ -1008,6 +1008,7 @@ text-align: center;
 </div>
 """
 st.markdown(footer,unsafe_allow_html=True)
+
 
 
 
